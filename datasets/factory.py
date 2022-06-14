cd6872618b6d9d0ae56ad8251568a62e6e1f5f51
Zhimin Sun <121097854@qq.com>
2022-06-14 12:50:18 +0800

    [Feat] Update RDD with STIL repo

---
+++ datasets/factory.py
@@ -0,0 +1,24 @@
+import sys
+import os
+from omegaconf import OmegaConf
+
+from common.data import create_base_transforms, create_base_dataloader
+
+def get_dataloader(args, split):
+    """Set dataloader.
+
+    Args:
+        args (object): Args load from get_params function.
+        split (str): One of ['train', 'test']
+    """
+    transform = create_base_transforms(args.transform_params, split=split)
+
+    dataset_cfg = getattr(args, split).dataset
+    dataset_params = OmegaConf.to_container(dataset_cfg.params, resolve=True)
+    dataset_params['transform'] = transform
+
+    _dataset = eval(dataset_cfg.name)(**dataset_params)
+
+    _dataloader = create_base_dataloader(args, _dataset, split=split)
+
+    return _dataloader
