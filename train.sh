1b1983317bad51e588243aaf5b0ccb995e1db316
duyxxd <duyxxd@umich.edu>
2022-06-22 22:13:28 +0000

    train script

--- train.sh
+++ train.sh
@@ -5,4 +5,4 @@ METHOD=NeuralTextures
 # video compression rate. One of ['c23', 'c40'].
 COMPRESSION=c40
 
-python3 -m torch.distributed.launch --nproc_per_node=8 --master_port 12345 train.py -c configs/ffpp.yaml --method ${METHOD} --compression ${COMPRESSION}
+python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12345 train.py -c configs/ffpp_2stream.yaml --method ${METHOD} --compression ${COMPRESSION}
