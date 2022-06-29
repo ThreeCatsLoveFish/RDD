cd9dd8ddfb86765775b5ec8da25cb95a8176d426
duyxxd <duyxxd@umich.edu>
2022-06-29 04:38:17 +0000

    run 12 exp

---
+++ task1.sh
@@ -0,0 +1,7 @@
+set -e
+
+CUDA_VISIBLE_DEVICES=0,1 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12346 train.py -c configs/ffpp_stil_1out.yaml --method Deepfakes --compression c40 && CUDA_VISIBLE_DEVICES=0,1 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12346 train.py -c configs/ffpp_stil_1out.yaml --method Face2Face --compression c40
+
+CUDA_VISIBLE_DEVICES=0,1 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12346 train.py -c configs/ffpp_x3d_1out.yaml --method Deepfakes --compression c40 && CUDA_VISIBLE_DEVICES=0,1 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12346 train.py -c configs/ffpp_x3d_1out.yaml --method Face2Face --compression c40
+
+CUDA_VISIBLE_DEVICES=0,1 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12346 train.py -c configs/ffpp_2stream_1out.yaml --method Deepfakes --compression c40 && CUDA_VISIBLE_DEVICES=0,1 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12346 train.py -c configs/ffpp_2stream_1out.yaml --method Face2Face --compression c40
