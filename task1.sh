5af48e00509d9283829fbbf56198d91a64a8fe98
duyxxd <duyxxd@umich.edu>
2022-07-07 03:50:49 +0000

    update train test setting

--- task1.sh
+++ task1.sh
@@ -1,4 +1,5 @@
 set -e
+set -x
 
 echo "CUDA_VISIBLE_DEVICES=2,3 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12346 train.py -c configs/ffpp_stil_1out.yaml --method Deepfakes --compression c40"
 CUDA_VISIBLE_DEVICES=2,3 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12346 train.py -c configs/ffpp_stil_1out.yaml --method Deepfakes --compression c40
--- task2.sh
+++ task2.sh
@@ -1,4 +1,5 @@
 set -e
+set -x
 
 echo "CUDA_VISIBLE_DEVICES=4,5 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12347 train.py -c configs/ffpp_stil_1out.yaml --method FaceSwap --compression c40"
 CUDA_VISIBLE_DEVICES=4,5 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12347 train.py -c configs/ffpp_stil_1out.yaml --method FaceSwap --compression c40
