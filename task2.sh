0fd0d70db3ed18dd7ccc5130955e83a5560b4663
duyxxd <duyxxd@umich.edu>
2022-07-12 21:14:08 +0000

    3to1 test

--- task2.sh
+++ task2.sh
@@ -1,14 +1,18 @@
 set -e
 set -x
 
-CUDA_VISIBLE_DEVICES=4,5 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12347 train.py -c configs/ffpp_stil_1out.yaml --method FaceSwap --compression c40
+# CUDA_VISIBLE_DEVICES=2,3 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12346 train.py -c configs/ffpp_stil_1out.yaml --method FaceSwap --compression c40
 
-CUDA_VISIBLE_DEVICES=4,5 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12347 train.py -c configs/ffpp_stil_1out.yaml --method NeuralTextures --compression c40
+# CUDA_VISIBLE_DEVICES=2,3 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12346 train.py -c configs/ffpp_stil_1out.yaml --method NeuralTextures --compression c40
 
-CUDA_VISIBLE_DEVICES=4,5 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12347 train.py -c configs/ffpp_x3d_1out.yaml --method FaceSwap --compression c40
+# CUDA_VISIBLE_DEVICES=2,3 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12346 train.py -c configs/ffpp_x3d_1out.yaml --method FaceSwap --compression c40
 
-CUDA_VISIBLE_DEVICES=4,5 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12347 train.py -c configs/ffpp_x3d_1out.yaml --method NeuralTextures --compression c40
+# CUDA_VISIBLE_DEVICES=2,3 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12346 train.py -c configs/ffpp_x3d_1out.yaml --method NeuralTextures --compression c40
 
-CUDA_VISIBLE_DEVICES=4,5 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12347 train.py -c configs/ffpp_2stream_1out.yaml --method FaceSwap --compression c40
+# CUDA_VISIBLE_DEVICES=2,3 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12346 train.py -c configs/ffpp_2stream_1out.yaml --method FaceSwap --compression c40
 
-CUDA_VISIBLE_DEVICES=4,5 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12347 train.py -c configs/ffpp_2stream_1out.yaml --method NeuralTextures --compression c40
+# CUDA_VISIBLE_DEVICES=2,3 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12346 train.py -c configs/ffpp_2stream_1out.yaml --method NeuralTextures --compression c40
+
+CUDA_VISIBLE_DEVICES=2,3 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12346 train.py -c configs/ffpp_2stream_pfreq_1out.yaml --method FaceSwap --compression c40
+
+CUDA_VISIBLE_DEVICES=2,3 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12346 train.py -c configs/ffpp_2stream_pfreq_1out.yaml --method NeuralTextures --compression c40
