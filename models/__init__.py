fca4b19347d87e5cb3dfae0f15d86d519916dad6
duyxxd <duyxxd@umich.edu>
2022-07-12 16:14:50 +0000

    patch 2stream

--- models/__init__.py
+++ models/__init__.py
@@ -5,3 +5,4 @@ from .x3d import X3D
 from .x3d2stream import X3D_2Stream
 from .patch_freq import PatchFreq
 from .patch_freq_reconst import PatchFreqReconst
+from .twostream_patch import TwoStreamPatchFreq
