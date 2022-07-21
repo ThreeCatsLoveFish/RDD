b4db23e65d1c90654fa1bf26a532175e848fc762
duyxxd <duyxxd@umich.edu>
2022-07-21 00:09:41 +0000

    move to val set

--- test.py
+++ test.py
@@ -50,7 +50,7 @@ def main():
     model.load_state_dict(sd)
 
     args.test.dataset.params.split = 'test'
-    test_dataloader = get_dataloader(args, 'test')
+    test_dataloader = get_dataloader(args, 'val')
 
     # main test function
     test(test_dataloader, model, args)
