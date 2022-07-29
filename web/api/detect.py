01b41c82dba62446318c63a654a63ac0631b4af8
Zhimin Sun <121097854@qq.com>
2022-07-29 21:35:26 +0800

    [Feat] Complete detection part

--- web/api/detect.py
+++ web/api/detect.py
@@ -1,9 +1,18 @@
 from flask import render_template, request, jsonify
 import subprocess
+import os
 
 
 def detect():
     path = request.form['video_path']
-    ps = subprocess.Popen(['python', 'inference.py', '--input', path], cwd='../model')
+    file = os.path.join('..', 'web', path)
+    ps = subprocess.Popen(['python', 'inference.py', '--input', file], cwd='../model', stdout=subprocess.PIPE)
     ps.wait()
-    return render_template('detect.html', video_path=path)
+    result = ps.stdout.readlines()
+    for line in result:
+        line = line.decode('utf-8')
+        if line.startswith('Result'):
+            detect =  line.split(';')[0].split(' ')[-1]
+            confidence = line.split(';')[1].split(' ')[-1]
+
+    return render_template('detect.html', detect=detect, confidence=confidence)
--- web/api/file.py
+++ web/api/file.py
@@ -1,4 +1,5 @@
 from flask import render_template, request, jsonify
+import uuid
 import os
 
 
@@ -13,8 +14,7 @@ def upload():
     f = request.files['file']
     if not (f and allowed_file(f.filename)):
         return jsonify({"error": 1001, "msg": "Please check the type of file, only mp4 is allowed!"})
-    upload_path = os.path.join('static', 'videos', f.filename)
-    print("name is %s, path is %s" % (f.filename, upload_path))
+    upload_path = os.path.join('static', 'videos', '{}.mp4'.format(uuid.uuid4()))
+    print("Filename is %s, path is %s" % (f.filename, upload_path))
     f.save(upload_path)
-    print("finished upload")
     return render_template('video_preview.html', video_path=upload_path)
